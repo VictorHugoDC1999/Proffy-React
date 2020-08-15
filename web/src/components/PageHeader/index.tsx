e44484a23dd26ab6021a4c8b2d4890baf2c97ede
VictorHugoDC1999 <victorhugodelciampo@gmail.com>
2020-08-15 11:10:40 -0300

    :heavy_plus_sign: Implementing Teacher Item

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -4,9 +4,14 @@ import { Link } from 'react-router-dom';
 import logoImg from '../../assets/images/logo.svg';
 import backIcon from '../../assets/images/icons/back.svg';
 
-/*componentes são funções que retornam HTML e propriedades são uma forma de nos conseguirmos passar informações para nosso componente*/
+import './styles.css';
+/*componentes são funções que retornam HTML e propriedades são uma forma de nos conseguirmos passar informações para nosso componente. No react a forma mais facil que temos para declarar as tipagens das propriedades e da forma que segue abaixo(em const PageHeader..). Agora (interface) estamos falando que o componente PageHeader pode receber uma propriedade title, que é uma string e que ela vai ser obrigatoria (uma tipagem)*/
 
-function PageHeader() {
+interface PageHeaderProps {/*caso a propriedade title não precisasse ser obrigatoria era só fazer fda seguinte maneira title?: string;*/
+    title: string;
+}
+
+const PageHeader: React.FC<PageHeaderProps> = (props) => {/*aqui estamos falando que o meu PageHeader é do tipo React Function Component e ele recebe um parametro que é a nossa PageHeaderProps*/
     return (
     <header className="page-header">
         <div className="top-bar-container">
@@ -17,8 +22,12 @@ function PageHeader() {
         </div>
         
         <div className="header-content">
-            <strong>Estes são os proffys disponíveis.</strong>
+            <strong>{props.title}</strong>
+            
+            {props.children}
         </div>
+
+        
     </header>
     );
 }
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -3,14 +3,39 @@ import React from 'react';
 import PageHeader from '../../components/PageHeader';
 
 import './styles.css';
+import TeacherItem from '../../components/TeacherItem';
 
 
 function TeacherList() {
     return (/*na linha 9 em to colocamos barra, pois queremos que ele retorne a pagina inicial*/
         <div id="page-teacher-list" className="container">
-            <PageHeader title="Estes são os proffys disponíveis."/>
+            <PageHeader title="Estes são os proffys disponíveis.">
+                <form id="search-teachers">
+                    <div className="input-block">
+                        <label htmlFor="subject">Matéria</label>
+                        <input type="text" id="subject" />
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="week_day">Dia da semana</label>
+                        <input type="text" id="subject" />
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="time">Hora</label>
+                        <input type="text" id="subject" />
+                    </div>
+                </form>
+            </PageHeader>
+
+            <main>
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+            </main>
         </div>
-    );/*PageHeader dentro da div é um componente, com suas propriedades. Temos que declarar as propriedades*/
+    );/*PageHeader dentro da div é um componente, com suas propriedades. Temos que declarar e adicionar tipagens para as propriedades. No react a forma mais facil que temos para declarar as tipagens das propriedades e da forma que segue na pasta PageHeader(index.tsx)*/
 }
 
 export default TeacherList;
